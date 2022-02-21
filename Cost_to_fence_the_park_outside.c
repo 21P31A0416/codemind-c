044831cacacf98931672d14d20f217f19459da68
21P31A0416 <98729946+21P31A0416@users.noreply.github.com>
2022-02-21 09:04:18 +0430

    #include<stdio.h> int main() { int l,b,w,c,area; scanf("%d%d%d%d",&l,&b,&w,&c); area=((l+2*w)*(b+2*w)-l*b); printf("%d",area*c); return 0; }

--- Cost_to_fence_the_park_outside.c
+++ Cost_to_fence_the_park_outside.c
@@ -1,15 +1,9 @@
-
 #include<stdio.h>
 int main()
 {
- int l,b,w,c,l1,b1,f;
- scanf("%d %d %d %d", &l,&b,&w,&c);
- l1=l+2*w;
- b1=b+2*w;
- f= ((l1*b1)-(l*b))*c;
- if(l1<=0 || b1<=0)
- printf("Impossible");
- else
- printf("%d",f);
+int l,b,w,c,area;
+scanf("%d%d%d%d",&l,&b,&w,&c);
+area=((l+2*w)*(b+2*w)-l*b);
+printf("%d",area*c);
 return 0;
 }
