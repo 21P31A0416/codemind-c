174376d3153727dd7880139a06c9b16882a1481d
21P31A0416 <98729946+21P31A0416@users.noreply.github.com>
2022-02-21 09:24:11 +0430

    #include<stdio.h>  int main() {     int n1,n2;     float avg;     scanf("%d%d",&n1,&n2);     avg=(float)(n1+n2)/2;     printf("%.4f",avg);     return 0; }

--- Find_the_average_of_two_numbers.c
+++ Find_the_average_of_two_numbers.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-int main()
+#include<stdio.h> 
+ int main()
 {
-float m,n,sum=0,average=0;
-scanf("%f%f",&m,&n);
-sum=m+n;
-average=sum/2;
-printf("%0.4f",average);
-return 0;
+    int n1,n2; 
+    float avg; 
+    scanf("%d%d",&n1,&n2); 
+    avg=(float)(n1+n2)/2; 
+    printf("%.4f",avg);
+    return 0;
 }
